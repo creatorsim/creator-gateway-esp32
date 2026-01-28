c286c1b5ec2aa21a464fbfaa7ef13342bf06977e
EUtrilla2002 <100451242@alumnos.uc3m.es>
2026-01-28 12:43:19 +0100

    Creatino stable version

--- esp32c3/tmp_assembly.s
+++ esp32c3/tmp_assembly.s
@@ -1,55 +1,114 @@
-#
-# ARCOS.INF.UC3M.ES
-# BY-NC-SA (https://creativecommons.org/licenses/by-nc-sa/4.0/deed.es)
-#
+#Creatino example: Semaphore with 3 LEDs
 
-
+.data
+    time:
+        .word 1000
 .text
+setup:
+    li a0,  4   #RED PIN GPIO
+    li a1,  0x03 
+    addi sp, sp, -4       
+    sw ra, 0(sp)    
+    jal ra, pinMode
+    lw ra, 0(sp)          
+    addi sp, sp, 4  
 
-main: 
-        addi sp, sp, -4
-        sw ra, 0(sp)
+    li a0,  5   #YELLOW PIN GPIO
+    li a1,  0x03 
+    addi sp, sp, -4       
+    sw ra, 0(sp)    
+    jal ra, pinMode
+    lw ra, 0(sp)          
+    addi sp, sp, 4 
 
-        # t1 = factorial(5)
-        li  a0, 5
-        jal x1, factorial
+    li a0,  7   #GREEN PIN GPIO
+    li a1,  0x03 
+    addi sp, sp, -4       
+    sw ra, 0(sp)    
+    jal ra, pinMode
+    lw ra, 0(sp)          
+    addi sp, sp, 4 
 
-        # print_int(t1)
-        li  a7, 1
-        ecall
+    jr ra
+loop:
+    ## ---------------------RED PIN---------------------
+    li a0,4 
+    li a1, 0x1
+    addi sp, sp, -4       
+    sw ra, 0(sp)      
+    jal ra, digitalWrite
+    lw ra, 0(sp)          
+    addi sp, sp, 4    
+    la a0, time
+    lw a0, 0(a0)
+    addi sp, sp, -16      
+    sw ra, 12(sp)
+    jal ra, delay
+    lw ra, 12(sp)          
+    addi sp, sp, 16 
+    li a0,4 
+    li a1, 0x0
+    jal ra, digitalWrite
+    la a0, time
+    lw a0, 0(a0)
+    addi sp, sp, -16      
+    sw ra, 12(sp)
+    jal ra, delay
+    lw ra, 12(sp)          
+    addi sp, sp, 16
 
-        # return
-        lw ra, 0(sp)
-        addi sp, sp, 4
-        li a7, 10
-        ecall
+    ## ---------------------Yellow PIN---------------------
+    li a0,5
+    li a1, 0x1
+    jal ra, digitalWrite
+    la a0, time
+    lw a0, 0(a0)
+    addi sp, sp, -16      
+    sw ra, 12(sp)
+    jal ra, delay
+    lw ra, 12(sp)          
+    addi sp, sp, 16 
+    li a0,5 
+    li a1, 0x0
+    jal ra, digitalWrite
+    la a0, time
+    lw a0, 0(a0)
+    addi sp, sp, -16      
+    sw ra, 12(sp)
+    jal ra, delay
+    lw ra, 12(sp)          
+    addi sp, sp, 16
 
+    ## ---------------------Green PIN---------------------
+    li a0,7 
+    li a1, 0x1
+    jal ra, digitalWrite
+    la a0, time
+    lw a0, 0(a0)
+    addi sp, sp, -16      
+    sw ra, 12(sp)
+    jal ra, delay
+    lw ra, 12(sp)          
+    addi sp, sp, 16 
+    li a0,7 
+    li a1, 0x0
+    jal ra, digitalWrite
+    la a0, time
+    lw a0, 0(a0)
+    addi sp, sp, -16      
+    sw ra, 12(sp)
+    jal ra, delay
+    lw ra, 12(sp)          
+    addi sp, sp, 16  
 
-factorial:
-        # crear "stack frame" para $ra, $fp y una variable local
-        addi sp, sp, -12
-        sw   ra, 8(sp)
-        sw   fp, 4(sp)
-        addi fp, sp, 4
-
-        # if (a0 < 2):
-        #     return 1
-        li   x5, 2
-        bge  a0, t0, b_else
-        li   a0, 1 
-        beq  x0, x0, b_efs
-        # else:
-        #    return a0 * factorial(a0 - 1)
-b_else: sw   a0, -4(fp)
-        addi a0, a0, -1
-        jal  x1, factorial
-        lw   t1, -4(fp)
-        mul  a0, a0, t1
-
-        # finalizar "stack frame"
-b_efs:  lw   ra, 8(sp)
-        lw   fp, 4(sp)
-        addi sp, sp, 12
-
-        # return t0
-        jr ra
+    j loop
+    
+main:
+    #Initialize Arduino
+    addi sp, sp, -16       
+    sw ra, 12(sp)          
+    jal ra, initArduino    
+    jal ra, setup
+    lw ra, 12(sp)          
+    addi sp, sp, 16             
+    j loop
